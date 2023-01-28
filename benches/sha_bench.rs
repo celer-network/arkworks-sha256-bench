004afd40f71e554aa140fd2b58b34c38c2646efb
Elliott He <elliott.he@outlook.com>
2023-01-28 10:48:18 +0800

    bench repeat

--- benches/sha_bench.rs
+++ benches/sha_bench.rs
@@ -2,6 +2,8 @@
 //     RAYON_NUM_THREADS=N cargo bench --no-default-features --features "std parallel" -- --nocapture
 // where N is the number of threads you want to use (N = 1 for single-thread).
 
+use std::env;
+
 use ark_bls12_381::Bls12_381;
 use ark_crypto_primitives::{
     crh::sha256::{
@@ -20,6 +22,7 @@ use ark_std::rand::{self, rngs::StdRng, RngCore};
 struct Sha256Circuit {
     pub data: Vec<u8>,
     pub expect: Vec<u8>,
+    pub num_repeats: usize,
 }
 
 impl Clone for Sha256Circuit {
@@ -27,6 +30,7 @@ impl Clone for Sha256Circuit {
         Sha256Circuit {
             data: self.data.as_slice().to_vec(),
             expect: self.expect.as_slice().to_vec(),
+            num_repeats: self.num_repeats.clone(),
         }
     }
 }
@@ -36,19 +40,26 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for Sha256Circuit {
         let data = UInt8::new_witness_vec(cs.clone(), &self.data).unwrap();
         let expect = UInt8::new_witness_vec(cs, &self.expect).unwrap();
 
-        let mut sha256_var = Sha256Gadget::default();
-        sha256_var.update(&data).unwrap();
+        for _ in 0..self.num_repeats {
+            let mut sha256_var = Sha256Gadget::default();
+            sha256_var.update(&data).unwrap();
 
-        sha256_var.finalize()?.enforce_equal(&DigestVar(expect))?;
+            sha256_var
+                .finalize()?
+                .enforce_equal(&DigestVar(expect.clone()))?;
+        }
 
         Ok(())
     }
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let num_repeats = args[1].parse::<usize>().unwrap();
+
     let mut rng = ark_std::test_rng();
     // Make a random string of the given length
-    let mut input_str = vec![0u8; 24];
+    let mut input_str = vec![0u8; 64];
     rng.fill_bytes(&mut input_str);
     let mut sha256 = Sha256::default();
     sha256.update(&input_str);
@@ -57,6 +68,7 @@ fn main() {
     let circuit = Sha256Circuit {
         data: input_str,
         expect,
+        num_repeats,
     };
 
     type GrothSetup = Groth16<Bls12_381>;
@@ -65,13 +77,15 @@ fn main() {
     let start = ark_std::time::Instant::now();
     let proof = GrothSetup::prove(&pk, circuit, &mut test_rng).unwrap();
     println!(
-        "per-constraint proving time for sha256: {} ms",
+        "per-constraint proving time for {} sha256: {} ms",
+        num_repeats,
         start.elapsed().as_millis()
     );
     let start = ark_std::time::Instant::now();
     let res = GrothSetup::verify(&vk, &vec![], &proof).unwrap();
     println!(
-        "verifying time for sha256: {} ms",
+        "verifying time for {} sha256: {} ms",
+        num_repeats,
         start.elapsed().as_millis()
     );
     assert!(res);
