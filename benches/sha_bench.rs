26ad21bd50f2944b4de4c94b431071beedebec7e
Elliott He <elliott.he@outlook.com>
2023-02-06 16:49:10 +0800

    update bench rule

--- benches/sha_bench.rs
+++ benches/sha_bench.rs
@@ -4,25 +4,20 @@
 
 use std::env;
 
-use ark_bls12_381::Bls12_381;
+use ark_bn254::Bn254;
 use ark_crypto_primitives::{
-    crh::sha256::{
-        constraints::{DigestVar, Sha256Gadget},
-        digest::Digest,
-        Sha256,
-    },
+    crh::sha256::constraints::{DigestVar, Sha256Gadget},
     snark::SNARK,
 };
 use ark_ff::{PrimeField, ToConstraintField};
 use ark_groth16::Groth16;
 use ark_r1cs_std::{prelude::EqGadget, uint8::UInt8};
 use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystemRef, SynthesisError};
-use ark_std::rand::{self, rngs::StdRng, RngCore};
+use ark_std::rand::{self, rngs::StdRng};
 
 struct Sha256Circuit {
     pub data: Vec<u8>,
     pub expect: Vec<u8>,
-    pub num_repeats: usize,
 }
 
 impl Clone for Sha256Circuit {
@@ -30,7 +25,6 @@ impl Clone for Sha256Circuit {
         Sha256Circuit {
             data: self.data.as_slice().to_vec(),
             expect: self.expect.as_slice().to_vec(),
-            num_repeats: self.num_repeats.clone(),
         }
     }
 }
@@ -40,18 +34,16 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for Sha256Circuit {
         let data = UInt8::new_witness_vec(cs.clone(), &self.data).unwrap();
         let expect = UInt8::new_input_vec(cs.clone(), &self.expect).unwrap();
 
-        for _ in 0..self.num_repeats {
-            let mut sha256_var = Sha256Gadget::default();
-            sha256_var.update(&data).unwrap();
+        let mut sha256_var = Sha256Gadget::default();
+        sha256_var.update(&data).unwrap();
 
-            sha256_var
-                .finalize()?
-                .enforce_equal(&DigestVar(expect.clone()))?;
-        }
+        sha256_var
+            .finalize()?
+            .enforce_equal(&DigestVar(expect.clone()))?;
 
         println!(
-            "num_constraints of {} sha256: {}",
-            self.num_repeats,
+            "num_constraints of sha256 with input size {} bytes : {}",
+            self.data.len(),
             cs.num_constraints()
         );
 
@@ -61,37 +53,31 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for Sha256Circuit {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let num_repeats = args[1].parse::<usize>().unwrap();
+    let num_of_64_bytes = args[1].parse::<usize>().unwrap();
+    let expect = hex::decode(args[2].parse::<String>().unwrap()).unwrap();
 
-    let mut rng = ark_std::test_rng();
-    // Make a random string of the given length
-    let mut input_str = vec![0u8; 64];
-    rng.fill_bytes(&mut input_str);
-    let mut sha256 = Sha256::default();
-    sha256.update(&input_str);
-    let expect = sha256.finalize().to_vec();
-
+    let input_size = 64 * num_of_64_bytes;
+    let input_str = vec![0u8; input_size];
     let circuit = Sha256Circuit {
         data: input_str,
         expect: expect.clone(),
-        num_repeats,
     };
 
-    type GrothSetup = Groth16<Bls12_381>;
+    type GrothSetup = Groth16<Bn254>;
     let mut test_rng = test_rng();
     let (pk, vk) = GrothSetup::circuit_specific_setup(circuit.clone(), &mut test_rng).unwrap();
     let start = ark_std::time::Instant::now();
     let proof = GrothSetup::prove(&pk, circuit, &mut test_rng).unwrap();
     println!(
-        "proving time for {} sha256: {} ms",
-        num_repeats,
+        "proving time for sha256 with input size {} bytes: {} ms",
+        input_size,
         start.elapsed().as_millis()
     );
     let start = ark_std::time::Instant::now();
     let res = GrothSetup::verify(&vk, &expect.to_field_elements().unwrap(), &proof).unwrap();
     println!(
-        "verifying time for {} sha256: {} ms",
-        num_repeats,
+        "verifying time for sha256 with input size {} bytes: {} ms",
+        input_size,
         start.elapsed().as_millis()
     );
     assert!(res);
